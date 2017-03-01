c2d1394fda4d7334ddd32d00f111ea72c920c194
Marcelo Severgnini <marcelosevergnini@gmail.com>
2017-03-01 22:46:52 +0000

    Added simple net client to test connection

--- src/js/controllers/chatController.js
+++ src/js/controllers/chatController.js
@@ -1,7 +1,36 @@
 var chatController = function ($scope, $http) {
 
+	var net = require('net');
+
+	var createClient = function(){
+
+		var client = net.connect(3000, '127.0.0.1');
+		client.on('connect', function () {
+			client.write('Hello, I am the client!');
+			$("#chat-alert-success").removeClass("hide--all");
+			$("#connect_chat").addClass("hide--all");
+			
+		});
+		client.on('data', function (message) {
+			console.log(message.toString());
+		});
+		client.on('end', function () {
+			process.exit();
+		});
+		process.stdin.on('readable', function () {
+			var message = process.stdin.read();
+			if (!message) return;
+			message = message.toString().replace(/\n/, '');
+			client.write(message);
+		});
+
+	}
 
 
+	$scope.connect = function(){
+		createClient();
+	}
+	
 }
 
-app.controller('chatController', chatController());
+module.controller('chatController', chatController);
